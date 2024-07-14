ffcca2fd46ce8e91086f7774d871bbf1e827b5dc
jdav892 <jadavila9@gmail.com>
2024-07-13 22:07:50 -0400

    another

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 from datetime import datetime
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+MY_LAT = 51.507351 #latitude
+MY_LONG = -0.127758 #longitude
 
 def iss_overhead():
     response = requests.get(url="http://api.open-notify.org/iss-now.json")
@@ -12,7 +12,7 @@ def iss_overhead():
     iss_latitude = float(data["iss_position"]["latitude"])
     iss_longitude = float(data["iss_position"]["longitude"])
 
-    #Your position is within +5 or -5 degrees of the ISS position.
+    #Position is within +5 or -5 degrees of the ISS position.
     if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
         return True
 
@@ -27,13 +27,13 @@ def is_night():
     data = response.json()
     sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
     sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
-
-time_now = datetime.now()
+    time_now = datetime.now()
+    if time_now >= sunset or time_now <= sunrise:
+        return True
 
 #If the ISS is close to my current position
 # and it is currently dark
 # Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
 
 
 
